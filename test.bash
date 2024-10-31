6aef844cb284710d620062c8a7b88985014c183d
Yuta Sakusabe <s23c1062mq@s.chibakoudai.jp>
2024-10-31 12:26:57 +0900

    test plus

--- test.bash
+++ test.bash
@@ -1,13 +1,15 @@
 #!/bin/bash
-
+# SPDX-FileCopyrightText: 2024 Yuta Sakusabe <s23c1062mq@s.chibakoudai.jp>
+# SPDX-License-Identifier: BSD-3-Clause
 ng () {
 	echo ${1}行目が違うよ
 	res=1
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
