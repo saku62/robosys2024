58268bb306637ac73dc06b4424ebb0b1f781d037
Yuta Sakusabe <s23c1062mq@s.chibakoudai.jp>
2024-11-26 10:37:28 +0900

    ?

--- kadai1_test.bash
+++ kadai1_test.bash
@@ -1,7 +1,21 @@
-#!/bin/bash
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2024 Yuta Sakusabe <s23c1062mq@s.chibakoudai.jp>
+# SPDX-License-Identifier: BSD-3-Clause
+import random
 
-a=山田
-[ "$a" = 上田 ]       
-echo $?               
-[ "$a" = 山田 ]
-echo $?
+
+categories = {
+    "tank":["D.VA"],
+    "dps":["アッシュ"],
+    "support":["アナ"]
+}
+
+
+user_input = input("Enter 'tank', 'support', or 'dps': ").strip().lower()
+
+
+if user_input in categories:
+    result = random.choice(categories[user_input])
+    print(f"{user_input}: {result}")
+else:
+    print("無効な入力です")
