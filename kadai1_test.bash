9aeab05e97f6d920bc0082b7c993b40cbc0273d3
Yuta Sakusabe <s23c1062mq@s.chibakoudai.jp>
2024-11-26 16:29:44 +0900

    ?

--- kadai1_test.bash
+++ kadai1_test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash
 # SPDX-FileCopyrightText: 2024 Yuta Sakusabe <s23c1062mq@s.chibakoudai.jp>
 # SPDX-License-Identifier: BSD-3-Clause
 ng(){
@@ -9,16 +9,16 @@ ng(){
 res=0
 
 #tank選択
-out=$(echo "tank" | ./kadai1_test.bash) 
-[ $out = "tank:" ] || ng "$LINENO"
+out=$(echo "tank" | ./kadai1)
+[[ $out =~ "tank:"* ]] || ng "$LINENO"
 
 #dps選択
-out=$(echo "dps" | ./kadai1_test.bash)
-[ $out = "dps:" ] || ng "$LINENO"
+out=$(echo "dps" | ./kadai1)
+[[ $out =~ "dps:"* ]] || ng "$LINENO"
 
 #support選択
-out=$(echo "support" | ./kadai1_test.bash)
-[ $out = "support:" ] || ng "$LINENO"
+out=$(echo "support" | ./kadai1)
+[[ $out =~ "support:"* ]] || ng "$LINENO"
 
 #実行成功
 [ "$res" = 0 ] && echo OK
