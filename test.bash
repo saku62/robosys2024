27f105000ffb8094f41df7b2d7d5118406016673
Yuta Sakusabe <s23c1062mq@s.chibakoudai.jp>
2024-11-27 19:42:37 +0900

    ?

--- test.bash
+++ test.bash
@@ -1,21 +1,25 @@
-#!/bin/bash -xv
+#!/bin/bash
 # SPDX-FileCopyrightText: 2024 Yuta Sakusabe <s23c1062mq@s.chibakoudai.jp>
 # SPDX-License-Identifier: BSD-3-Clause
-ng () {
-	echo ${1}行目が違うよ
-	res=1
+ng(){
+     echo ${1}行目が違うよ
+     res=1
 }
 
 res=0
 
-###正常な入力###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+#tank選択
+out=$(echo "tank" | ./hero_select)
+[[ $out =~ "tank:" ]] || ng "$LINENO"
 
-###変な入力###
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+#dps選択
+out=$(echo "dps" | ./hero_select)
+[[ $out =~ "dps:" ]] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
+#support選択
+out=$(echo "support" | ./hero_select)
+[[ $out =~ "support:" ]] || ng "$LINENO"
+
+#実行成功
+[ "$res" = 0 ] && echo OK
 exit $res
