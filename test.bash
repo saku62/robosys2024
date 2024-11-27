699094cfde77c23c6d844908d12d084669c8abb8
Yuta Sakusabe <s23c1062mq@s.chibakoudai.jp>
2024-11-27 23:45:46 +0900

    ?

--- test.bash
+++ test.bash
@@ -3,22 +3,23 @@
 # SPDX-License-Identifier: BSD-3-Clause
 ng(){
      echo ${1}行目が違うよ
+     echo "実際の出力: $out"
      res=1
 }
 
 res=0
 
 #tank選択
-out=$(echo "tank" | ./hero_select)
-[[ $out =~ "tank:" ]] || ng "$LINENO"
+out=$(./hero_select tank)
+[[ "$out" =~ tank ]] || ng "$LINENO"
 
 #dps選択
-out=$(echo "dps" | ./hero_select)
-[[ $out =~ "dps:" ]] || ng "$LINENO"
+out=$(./hero_select dps)
+[[ "$out" =~ dps ]] || ng "$LINENO"
 
 #support選択
-out=$(echo "support" | ./hero_select)
-[[ $out =~ "support:" ]] || ng "$LINENO"
+out=$(./hero_select support)
+[[ "$out" =~ support ]] || ng "$LINENO"
 
 #実行成功
 [ "$res" = 0 ] && echo OK
